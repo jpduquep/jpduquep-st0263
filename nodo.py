16952327bc3f95ea459de39aa100cedbcc50df77
jpduquep <jpduquep@eafit.edu.co>
2024-09-01 11:41:28 -0500

    Mensajeria en formato JSON, envia y recibe de manera correcta hacia izq y der

--- nodo.py
+++ nodo.py
@@ -3,7 +3,7 @@ import threading
 import sys
 import json
 
-listaDocumentos = [("titulo,informacion"),]
+listaDocumentos = [("titulo","informacion"),]
 
 def recibirMensajes(sock):
     #Funcion para recibir y gestionar los mensajes
@@ -19,67 +19,72 @@ def enviarMensajes(sock, puertoIzq, puertoDer, host='127.0.0.1'):
     
     
     while True:
-        mensaje = input('Digite salir si desea terminar la consola \n1: Para cargar un documento \n2: Para descargar un documento\n3:Mostrar lista de documentos')
-        if mensaje.lower() == "salir":
+        resp = input('Digite salir si desea terminar la consola \n1: Para cargar un documento \n2: Para descargar un documento\n3: Mostrar lista de documentos\nR/ ')
+        if resp.lower() == "salir":
             sock.close()
             break
 
 
         #En el 1 y 2 hay que armar el JSON para enviarlo como un API.
 
-        elif mensaje == '1': #Cargar documento a la red
+        elif resp == '1': #Cargar documento a la red
+            titulo = input("Digitia el nombre del documento que vas a ingresar: ")
+            mensaje = input("Cual es el contenido: ")
+            mensajeJson = json.dumps(
+                {
+                    "tipoMensaje" : None,
+                    "idDestinatario": None,
+                    "idRemitente": None,
+                    "titulo" : titulo,
+                    "mensaje":mensaje
+
+                }
+            )
+            #Enviando mensaje a la izquierda
+            if puertoIzq:
+                sock.sendto(mensajeJson.encode('utf-8'), (host, puertoIzq))
+                print(f"Mensaje enviado a la izquierda (puerto",puertoIzq,")")
+        
+            #Enviando mensaje a la derecha
+            if puertoDer:
+                sock.sendto(mensajeJson.encode('utf-8'), (host, puertoDer))
+                print(f"Mensaje enviado a la derecha (puerto",puertoDer,")")
             
+
+        elif resp == '2': #Descargar documento de la red
+            titulo = input("Digita el nombre del documento que buscas con extension: ")
             mensajeJson = json.dumps(
-            {
-                "tipoMensaje" : None,
-                "idDestinatario": None,
-                "idRemitente": None,
-                "titulo" : None,
-                "mensaje":mensaje
+                {
+                    "tipoMensaje" : None,
+                    "idDestinatario": None,
+                    "idRemitente": None,
+                    "titulo" : titulo,
+                    "mensaje":None
 
-            }
-        )
-            pass
+                }
+            )
+            #Enviando mensaje a la izquierda
+            if puertoIzq:
+                sock.sendto(mensajeJson.encode('utf-8'), (host, puertoIzq))
+                print(f"Mensaje enviado a la izquierda (puerto",puertoIzq,")")
+        
+            #Enviando mensaje a la derecha
+            if puertoDer:
+                sock.sendto(mensajeJson.encode('utf-8'), (host, puertoDer))
+                print(f"Mensaje enviado a la derecha (puerto",puertoDer,")")
+            
 
-        elif mensaje == 2: #Descargar documento de la red
-            mensajeJson = json.dumps(
-            {
-                "tipoMensaje" : None,
-                "idDestinatario": None,
-                "idRemitente": None,
-                "titulo" : None,
-                "mensaje":mensaje
-
-            }
-        )
-            pass
-
-        elif mensaje == 3:  #Mostrar listado
+        elif resp == '3':  #Mostrar listado
+            print("\nListado de documentos de este nodo:")
+            print("-"*20)
             for documento in listaDocumentos:
                 titulo,informacion = documento[0],documento[1]
                 print("Titulo: ",titulo)
-                print("Informacion:\n",informacion)
+                print("Informacion:\n"+informacion)
                 print("-"*20)
-
-
-
+            print("\n")
 
         
-        
-        
-
-        
-        
-
-        #Enviando mensaje a la izquierda
-        if puertoIzq:
-            sock.sendto(mensaje.encode('utf-8'), (host, puertoIzq))
-            print(f"Mensaje enviado a la izquierda (puerto",puertoIzq,")")
-        
-        #Enviando mensaje a la derecha
-        if puertoDer:
-            sock.sendto(mensaje.encode('utf-8'), (host, puertoDer))
-            print(f"Mensaje enviado a la derecha (puerto",puertoDer,")")
 
 def iniciarNodo(puertoDeEscucha, puertoIzq, puertoDer):
     #Iniciando Nodo, inicializa la escucha y la escritura
